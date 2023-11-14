8b69c49fbba1c9b73c7673eda1e2a14863ec75ba
Benjamin Pachevie <b.pachevie@gmail.com>
2023-11-14 17:06:51 +0100

    implementing fetching data from backend

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,14 @@ app.use(express.static('images'));
 app.use(bodyParser.json());
 
 // CORS
+console.log("Middleware body-parser configuré avec succès")
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); // allow all domains
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+  console.log('Serveur en cours d\'écoute sur le port 3000');
   next();
 });
 
@@ -22,7 +24,7 @@ app.get('/places', async (req, res) => {
   const fileContent = await fs.readFile('./data/places.json');
 
   const placesData = JSON.parse(fileContent);
-
+  console.log('Serveur en cours d\'écoute sur le port 3000');
   res.status(200).json({ places: placesData });
 });
 
@@ -30,15 +32,18 @@ app.get('/user-places', async (req, res) => {
   const fileContent = await fs.readFile('./data/user-places.json');
 
   const places = JSON.parse(fileContent);
-
+  console.log('Serveur en cours d\'écoute sur le port 3000');
   res.status(200).json({ places });
 });
+
+  console.log('Serveur en cours d\'écoute sur le port 3000');
+
 
 app.put('/user-places', async (req, res) => {
   const places = req.body.places;
 
   await fs.writeFile('./data/user-places.json', JSON.stringify(places));
-
+  console.log('Serveur en cours d\'écoute sur le port 3000');
   res.status(200).json({ message: 'User places updated!' });
 });
 
@@ -51,3 +56,4 @@ app.use((req, res, next) => {
 });
 
 app.listen(3000);
+console.log('Fin du fichier app.js');
